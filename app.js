89764f3eeeca584bd8ed997f868eef3864dc860c
zhaopx <zhaopengxiang@tocel.com>
2020-07-24 17:47:05 +0800

    0724登录

--- app.js
+++ app.js
@@ -5,7 +5,30 @@ App({
     baseUrl:"http://192.168.1.142:9000/CMTGP/"
   },
   onLaunch: function () {
-
+    var baseUrl = this.globalData.baseUrl;
+    wx.login({
+      success: (res) => {
+        var paras = {};
+        paras.code = res.code;
+        paras.type=2;
+        wx.request({
+          url: baseUrl+"supplier/getOpendId",
+          method: 'get',
+          data: paras,
+          success(res) {
+            console.info(res)
+            if(res.data.code==200){
+              var openid = res.data.data;
+              wx.setStorageSync('openid', openid)
+            }else{
+              wx.showToast({
+                title: "获取参数异常"
+              })
+            }
+          }
+        })
+      },
+    })
   },
    
 })
--- pages/login/login.js
+++ pages/login/login.js
@@ -5,25 +5,50 @@ Page({
    * 组件的初始数据
    */
   data: {
-
+    baseUrl:''
   },
   onLoad:function(){
-    var baseUrl = app.globalData.baseUrl;
-    var paras= {};
-    paras.account='tyxg';
-    paras.password='tyxg2020';
+    this.setData({
+      baseUrl:app.globalData.baseUrl
+    })
+  },
+  login:function(e){
+    var baseUrl = this.data.baseUrl;
+    var account = e.detail.value.account;
+    var pwd = e.detail.value.pwd;
+    var paras = {};
+    paras.account = account;
+    paras.password = pwd;
     wx.request({
       url: baseUrl+"supplier/login",
       method: 'get',
       data: paras,
       success(res) {
-        wx.switchTab({
-          url: '/pages/index/index',
-        })
+        console.info(res)
+        if(res.data.code==200){
+          wx.requestSubscribeMessage({
+            tmplIds: ['oGUeI8FFPely9OFgNkukIKzlVQ7Ze8uiBQ5BIHCxLS0','HFb7VThCuI0TK8W38LR1oPTv8wL8dxzJKTeF11eUce4'],
+            success (res) { 
+              if(res['oGUeI8FFPely9OFgNkukIKzlVQ7Ze8uiBQ5BIHCxLS0-ZwAFL-b3kALcl0c']=='accept' && res['HFb7VThCuI0TK8W38LR1oPTv8wL8dxzJKTeF11eUce4']=='accept'){
+                
+              }
+            }
+          })
+          wx.switchTab({
+            url: '/pages/index/index'
+          })
+        }else{
+          wx.showToast({
+            title: "账号或密码有误"
+          })
+        }
+       
       },fail(res){
-
+        wx.showToast({
+          title: "服务器异常"
+        })
       }
     })
+    
   }
-
 })
